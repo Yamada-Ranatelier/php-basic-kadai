e1ee4dc1267c9776ed9db89021f5aedadc9f401b
Yamada-Ranatelier <159823010+Yamada-Ranatelier@users.noreply.github.com>
2024-04-17 12:53:18 +0900

    7章課題

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_7章課題</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      $product_date = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+      print_r($product_date);
+      ?>
+    </p>
+  </body>
+</html>
